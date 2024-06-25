1d5af8a155d4d6672acdfa4aef96ddcb42f92686
HungDevPy <alonedeveloper203@gmail.com>
2024-06-25 13:16:38 +0700

    demo-web-fullstack-php

---
+++ baocaodoan (2).sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 19, 2024 lúc 04:00 AM
+-- Phiên bản máy phục vụ: 10.4.28-MariaDB
+-- Phiên bản PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `baocaodoan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admin`
+--
+-- Error reading structure for table baocaodoan.admin: #1932 - Table 'baocaodoan.admin' doesn't exist in engine
+-- Error reading data for table baocaodoan.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `binh_luan`
+--
+-- Error reading structure for table baocaodoan.binh_luan: #1932 - Table 'baocaodoan.binh_luan' doesn't exist in engine
+-- Error reading data for table baocaodoan.binh_luan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`binh_luan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danhmuc`
+--
+-- Error reading structure for table baocaodoan.danhmuc: #1932 - Table 'baocaodoan.danhmuc' doesn't exist in engine
+-- Error reading data for table baocaodoan.danhmuc: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`danhmuc`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `dathang`
+--
+-- Error reading structure for table baocaodoan.dathang: #1932 - Table 'baocaodoan.dathang' doesn't exist in engine
+-- Error reading data for table baocaodoan.dathang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`dathang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `doanhthu`
+--
+-- Error reading structure for table baocaodoan.doanhthu: #1932 - Table 'baocaodoan.doanhthu' doesn't exist in engine
+-- Error reading data for table baocaodoan.doanhthu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`doanhthu`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `doitac`
+--
+-- Error reading structure for table baocaodoan.doitac: #1932 - Table 'baocaodoan.doitac' doesn't exist in engine
+-- Error reading data for table baocaodoan.doitac: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`doitac`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `donhang`
+--
+-- Error reading structure for table baocaodoan.donhang: #1932 - Table 'baocaodoan.donhang' doesn't exist in engine
+-- Error reading data for table baocaodoan.donhang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`donhang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+-- Error reading structure for table baocaodoan.khachhang: #1932 - Table 'baocaodoan.khachhang' doesn't exist in engine
+-- Error reading data for table baocaodoan.khachhang: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`khachhang`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `payments`
+--
+-- Error reading structure for table baocaodoan.payments: #1932 - Table 'baocaodoan.payments' doesn't exist in engine
+-- Error reading data for table baocaodoan.payments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`payments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+-- Error reading structure for table baocaodoan.sanpham: #1932 - Table 'baocaodoan.sanpham' doesn't exist in engine
+-- Error reading data for table baocaodoan.sanpham: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`sanpham`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `taikhoan`
+--
+-- Error reading structure for table baocaodoan.taikhoan: #1932 - Table 'baocaodoan.taikhoan' doesn't exist in engine
+-- Error reading data for table baocaodoan.taikhoan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`taikhoan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `thanhtoan`
+--
+-- Error reading structure for table baocaodoan.thanhtoan: #1932 - Table 'baocaodoan.thanhtoan' doesn't exist in engine
+-- Error reading data for table baocaodoan.thanhtoan: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`thanhtoan`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tin_tuc`
+--
+-- Error reading structure for table baocaodoan.tin_tuc: #1932 - Table 'baocaodoan.tin_tuc' doesn't exist in engine
+-- Error reading data for table baocaodoan.tin_tuc: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `baocaodoan`.`tin_tuc`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
